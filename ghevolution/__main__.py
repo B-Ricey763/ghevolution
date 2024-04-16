d45d76317f1c6db16b4e287237a6f1bb8c42f45c
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2024-04-16 11:05:33 -0400

    Updated and fixed for mac

--- ghevolution/__main__.py
+++ ghevolution/__main__.py
@@ -11,20 +11,20 @@ import survivor_plot
 
 
 if __name__ == "__main__":
-    survivor_plot.init_graph()
-    plt.ion()
-    plt.show(block=False)
+    # survivor_plot.init_graph()
+    # plt.ion()
+    # plt.show(block=False)
     while True:
         genes = [genome.generate() for _ in range(NUM_ORGANISMS)]
         gen_num = list(range(NUM_GENERATIONS))
         survivor_list = list()
         for i in range(NUM_GENERATIONS):
             (genes, num_s, best_gene) = world.start_generation(genes, i)
-            survivor_plot.dyn_graph_survivors(gen_num[:i], survivor_list)
+            # survivor_plot.dyn_graph_survivors(gen_num[:i], survivor_list)
             survivor_list.append(num_s)
-            brain_view.dyn_drawn_brain(best_gene)
+            # brain_view.dyn_drawn_brain(best_gene)
         bounds.cycle_bounds()
-        survivor_plot.clear_graph()
+        # survivor_plot.clear_graph()
 
     # brain_view.draw_brain(best_gene)
     # survivor_plot.show_gen_survivors(gen_num, survivor_list)
